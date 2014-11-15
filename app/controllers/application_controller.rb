0f73f28a6c648b5702ff16f2fa4890cf94e4c757
acarolinatavares <Res!768837>
2014-11-14 22:20:59 -0200

    Alteracoes

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
+
+  def home
+    #@users = User.all
+    @noticias = Noticia.limit(5).order("created_at desc")
+    @noticia = Noticia.new
+  end
+
+  def contato
+    ContatoMailer.contato(params)
+    redirect_to :root, :notice => "Sua mensagem foi enviada com sucesso. Aguarde nosso retorno."
+  end
+
+end
