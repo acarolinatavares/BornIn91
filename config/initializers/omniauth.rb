0881a4c80f0f7d6a3273e6bdce0a3cafba2ab3d1
acarolinatavares <Res!768837>
2014-11-16 22:54:27 -0200

    Login via Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], :scope => "email"
-  provider :twitter, 'TFbht02WdpuDPQWQHgGTET96W', 'ueN8mbr1RvT8CSxKXcIOf4bW8GHTbchuLQE7sejtVVDPxk96w8'
+  provider :twitter, TWITTER_CONFIG['consumer_key'], TWITTER_CONFIG['consumer_secret']
 end
